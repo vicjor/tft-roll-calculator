9fa80c6ea1b7521a2ab89e0ff40d539e06da0ef4
Victor Jørgensen <victor.jorgens@gmail.com>
2024-05-30 19:30:27 +0200

    Fix calculation to simulate shops with 5 units in each

--- components/tft-calculator.tsx
+++ components/tft-calculator.tsx
@@ -1,15 +1,24 @@
-'use client'
-import React, { useState } from 'react';
-import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
-import { parse } from 'path';
-
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 
 type OddsTableType = Record<number, Record<number, number>>;
-const ODDS_TABLE: OddsTableType  = {
+const ODDS_TABLE: OddsTableType = {
   2: { 1: 100, 2: 0, 3: 0, 4: 0, 5: 0 },
   3: { 1: 75, 2: 25, 3: 0, 4: 0, 5: 0 },
   4: { 1: 55, 2: 30, 3: 15, 4: 0, 5: 0 },
@@ -19,7 +28,7 @@ const ODDS_TABLE: OddsTableType  = {
   8: { 1: 18, 2: 27, 3: 32, 4: 20, 5: 3 },
   9: { 1: 15, 2: 20, 3: 25, 4: 30, 5: 10 },
   10: { 1: 5, 2: 10, 3: 20, 4: 40, 5: 25 },
-  11: { 1: 1, 2: 2, 3: 12, 4: 50, 5: 35 }
+  11: { 1: 1, 2: 2, 3: 12, 4: 50, 5: 35 },
 };
 
 const CHAMPIONS_POOL: Record<number, number> = {
@@ -27,33 +36,33 @@ const CHAMPIONS_POOL: Record<number, number> = {
   2: 20,
   3: 17,
   4: 10,
-  5: 9
+  5: 9,
 };
 
 const LEVELS = [3, 4, 5, 6, 7, 8, 9, 10, 11];
 const TIERS = [1, 2, 3, 4, 5];
 
 const TIER_STYLES: Record<number, string> = {
-  1: 'bg-gray-300',    // Common
-  2: 'bg-green-300',   // Uncommon
-  3: 'bg-blue-300',    // Rare
-  4: 'bg-purple-300',  // Epic
-  5: 'bg-orange-300'   // Legendary
+  1: "bg-gray-300", // Common
+  2: "bg-green-300", // Uncommon
+  3: "bg-blue-300", // Rare
+  4: "bg-purple-300", // Epic
+  5: "bg-orange-300", // Legendary
 };
 const TIER_COLORS: Record<number, string> = {
-  1: 'gray',    // Common
-  2: 'green',   // Uncommon
-  3: 'blue',    // Rare
-  4: 'purple',  // Epic
-  5: 'orange'   // Legendary
+  1: "gray", // Common
+  2: "green", // Uncommon
+  3: "blue", // Rare
+  4: "purple", // Epic
+  5: "orange", // Legendary
 };
 
 const TFTCalculator = () => {
   const [level, setLevel] = useState("3");
   const [tier, setTier] = useState("");
   const [gold, setGold] = useState("50");
-  const [unitsOut, setUnitsOut] = useState('0');
-  const [totalUnitsOut, setTotalUnitsOut] = useState('0');
+  const [unitsOut, setUnitsOut] = useState("0");
+  const [totalUnitsOut, setTotalUnitsOut] = useState("0");
   const [chance, setChance] = useState<number | null>(null);
   const [error, setError] = useState<string | null>(null);
 
@@ -64,30 +73,36 @@ const TFTCalculator = () => {
     const parsedTier = parseInt(tier, 10);
     const poolSize = CHAMPIONS_POOL[parsedTier];
 
-    if (!isNaN(parsedGold) && parsedGold >= 2 && !isNaN(parsedUnitsOut) && !isNaN(parsedTotalUnitsOut)) {
+    if (
+      !isNaN(parsedGold) &&
+      parsedGold >= 2 &&
+      !isNaN(parsedUnitsOut) &&
+      !isNaN(parsedTotalUnitsOut)
+    ) {
       const rolls = Math.floor(parsedGold / 2);
       const odds = ODDS_TABLE[parseInt(level)][parsedTier];
       const availableUnits = poolSize - parsedUnitsOut;
       const totalUnits = poolSize * 13 - parsedTotalUnitsOut;
       const unitChance = (odds / 100) * (availableUnits / totalUnits);
-      const result = 1 - Math.pow((1 - unitChance), rolls);
+
+      const singleShopChance = 1 - Math.pow(1 - unitChance, 5);
+      const result = 1 - Math.pow(1 - singleShopChance, rolls);
+
       result < 0 ? setChance(0) : setChance(result * 100);
-      console.log("if", result, chance)
       setError(null);
     } else {
-      console.log("else")
       setChance(null);
       setError("Fill out all fields with valid numbers.");
     }
-  }
+  };
 
   const handleSelectLevelChange = (value: string) => {
     setLevel(value);
-  }
+  };
 
   const handleSelectTierChange = (value: string) => {
     setTier(value);
-  }
+  };
 
   const tierColorStyle = TIER_STYLES[parseInt(tier)];
 
@@ -97,41 +112,43 @@ const TFTCalculator = () => {
         <CardHeader>
           <h2 className="text-xl font-semibold">TFT Roll Calculator</h2>
         </CardHeader>
-        <CardContent className='grid gap-4 mb-4 w-full max-w-md'>
+        <CardContent className="grid gap-4 mb-4 w-full max-w-md">
           <Label htmlFor="level">Select your level</Label>
           <Select onValueChange={handleSelectLevelChange}>
-          <SelectTrigger>
-            <SelectValue placeholder="Select a level" />
-          </SelectTrigger>
-          <SelectContent>
-
-            {LEVELS.map((lvl) => (
-              <SelectItem key={lvl} value={lvl.toString()}>
-                Level {lvl}
-              </SelectItem>
-            ))}
+            <SelectTrigger>
+              <SelectValue placeholder="Select a level" />
+            </SelectTrigger>
+            <SelectContent>
+              {LEVELS.map((lvl) => (
+                <SelectItem key={lvl} value={lvl.toString()}>
+                  Level {lvl}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
 
           <Label htmlFor="level">Tier of desired unit to hit</Label>
           <Select onValueChange={handleSelectTierChange}>
-          <SelectTrigger className={tierColorStyle}>
-            <SelectValue placeholder="Select tier" />
-          </SelectTrigger>
-          <SelectContent>
-
-            {TIERS.map((tier) => (
-              <SelectItem key={tier} value={tier.toString()}  style={{color: TIER_COLORS[(tier)], fontWeight: 500}}>
-                Tier {tier}
-              </SelectItem>
-            ))}
+            <SelectTrigger className={tierColorStyle}>
+              <SelectValue placeholder="Select tier" />
+            </SelectTrigger>
+            <SelectContent>
+              {TIERS.map((tier) => (
+                <SelectItem
+                  key={tier}
+                  value={tier.toString()}
+                  style={{ color: TIER_COLORS[tier], fontWeight: 500 }}
+                >
+                  Tier {tier}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
-
 
           <Label htmlFor="gold">Enter your gold</Label>
           <Input
             id="gold"
+            type="number"
             placeholder="Gold"
             value={gold}
             onChange={(e) => setGold(e.target.value)}
@@ -139,10 +156,13 @@ const TFTCalculator = () => {
           />
 
           <div className="mb-4">
-            <Label htmlFor="units-out">Total units of desired champion already taken</Label>
+            <Label htmlFor="units-out">
+              Total units of desired champion already taken
+            </Label>
             <Input
               max={CHAMPIONS_POOL[parseInt(tier)] ?? 22}
               id="units-out"
+              type="number"
               placeholder="Units out"
               value={unitsOut}
               onChange={(e) => setUnitsOut(e.target.value)}
@@ -150,10 +170,13 @@ const TFTCalculator = () => {
             />
           </div>
           <div className="mb-4">
-            <Label htmlFor="total-units-out">Total {tier ? tier : "X"} cost units out of the pool</Label>
+            <Label htmlFor="total-units-out">
+              Total {tier ? tier : "X"} cost units out of the pool
+            </Label>
             <Input
-              max={CHAMPIONS_POOL[parseInt(tier)]*13 ?? 22}
+              max={CHAMPIONS_POOL[parseInt(tier)] * 13 ?? 22}
               id="total-units-out"
+              type="number"
               placeholder={`Total ${tier ? tier : "X"} cost units out`}
               value={totalUnitsOut}
               onChange={(e) => setTotalUnitsOut(e.target.value)}
@@ -161,19 +184,20 @@ const TFTCalculator = () => {
             />
           </div>
 
-          <Button onClick={handleCalculate} className="w-full">Calculate</Button>
+          <Button onClick={handleCalculate} className="w-full">
+            Calculate
+          </Button>
         </CardContent>
-          <CardFooter>
-            <div className="text-xl">
-              {(error === null && chance !== null) ? 
-                `Chance to hit a desired unit: ${chance.toFixed(2)}%` 
-                : error 
-              }
-            </div>
-          </CardFooter>
+        <CardFooter>
+          <div className="text-xl">
+            {error === null && chance !== null
+              ? `Chance to hit a desired unit: ${chance.toFixed(2)}%`
+              : error}
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
-}
+};
 
 export default TFTCalculator;
